ab00b36843b778814a43827cce6903f80188b352
Vova <volodimir979@gmail.com>
2022-12-02 14:58:27 +0200

    Del comments

--- framework/page.py
+++ framework/page.py
@@ -3,13 +3,13 @@ class Page:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, *locator):  # пошук елементу
+    def find_element(self, *locator):
         return self.driver.find_element(*locator)
 
-    def click_element(self, *locator):  # клік на елемент
+    def click_element(self, *locator):
         self.find_element(*locator).click()
 
-    def input(self, text, *locator):  # вставка тексту в поле
+    def input(self, text, *locator):
         e = self.find_element(*locator)
         e.clear()
         e.send_keys(text)
